c83e21b2d9914974291af5f27b0630326408c420
wzmyyj <2209011667@qq.com>
2019-09-29 14:34:24 +0800

    Initial commit

---
+++ common/src/main/java/top/wzmyyj/common/CommonModuleConfig.kt
@@ -0,0 +1,11 @@
+package top.wzmyyj.common
+
+/**
+ * Created on 2019/09/26.
+ *
+ * @author feling
+ * @version 1.0
+ * @since 1.0
+ */
+class CommonModuleConfig {
+}
---
+++ common/src/main/java/top/wzmyyj/common/base/CBase.kt
@@ -0,0 +1,9 @@
+package top.wzmyyj.common.base
+
+/**
+ * Created on 2019/09/26.
+ *
+ * @author feling
+ * @version 1.0
+ * @since 1.0
+ */
